fd16739eab7cfc88cd08f72555994b4c0fbe9341
chartrat <chartrat.m@gmail.com>
2026-06-03 21:46:43 +0700

    fix richmenus import path

--- src/app/api/user/bind-role/route.js
+++ src/app/api/user/bind-role/route.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "../../../../lib/supabase.js"
 import { RICHMENUS } from "../../../../lib/richmenus"
 
 const supabase = createClient(
