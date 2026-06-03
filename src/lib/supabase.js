953dbba359534adbce73e28a871bda14778bbf74
chartrat <chartrat.m@gmail.com>
2026-06-03 21:54:43 +0700

    fix supabase createClient conflict

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://mbdwfdhseagojrvbqqaq.supabase.co'
 const supabaseKey = 'sb_publishable_444YhvaiYTaWlUGOn_0s_w_tVKERi7Q'
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const createClient = () => {
+  return createClient(supabaseUrl, supabaseKey)
+}
